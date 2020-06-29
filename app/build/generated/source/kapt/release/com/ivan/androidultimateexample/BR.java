56f06366e455f8de2c89bcfcd2adba9a2e01457a
IvanSimovic <ivan.simovic@klika.ba>
2020-06-29 12:23:15 +0200

    clean

--- app/build/generated/source/kapt/release/com/ivan/androidultimateexample/BR.java
+++
@@ -1,9 +0,0 @@
-package com.ivan.androidultimateexample;
-
-public class BR {
-  public static final int _all = 0;
-
-  public static final int rowModel = 1;
-
-  public static final int viewModel = 2;
-}
